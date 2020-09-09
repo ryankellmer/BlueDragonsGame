a07e81878b5dd9f0bb9470fbf1d3da3e5d7c149a
Ronan <ronanpanopio98@gmail.com>
2020-09-08 18:57:59 -0700

    Added enemy movement via waypoints across example map
    Currently using Melissa's prefab for tower and projectile. Shoots my enemy but does not apply damage as it is using her enemycontroller instead of mine. Do not want to edit her prefabs or scripts. We can merge the two at some point.

---
+++ Assets/Scripts/ronan_EnemyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ronan_EnemyController : MonoBehaviour
+{
+	public GameObject[] waypoints;
+	private int currentWaypoint = 0;
+	private float lastWaypointSwitchTime;
+	public float speed = 1.0f;
+
+    Rigidbody2D rigidbody2D;
+
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startPos = waypoints [currentWaypoint].transform.position;
+        Vector3 endPos = waypoints [currentWaypoint + 1].transform.position;
+
+        float pathLength = Vector3.Distance (startPos, endPos);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time- lastWaypointSwitchTime;
+        gameObject.transform.position = Vector2.Lerp (startPos, endPos, currentTimeOnPath / totalTimeForPath);
+
+        if (gameObject.transform.position.Equals(endPos))
+        {
+        	if (currentWaypoint < waypoints.Length -2)
+        	{
+        		currentWaypoint++;
+        		lastWaypointSwitchTime = Time.time;
+        	}
+
+        	else
+        	{
+        		Destroy(gameObject);
+        	}
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UnityEngine.Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
---
+++ Assets/Scripts/spawnEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnEnemy : MonoBehaviour
+{
+	public GameObject[] waypoints;
+	public GameObject testEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(testEnemy).GetComponent<ronan_EnemyController>().waypoints = waypoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
