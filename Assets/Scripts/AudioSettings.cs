d916bb4819cc63a68a5ae93ccc396291ef526b16
ZionJones <Joneszion412@gmail.com>
2020-09-25 22:33:31 -0700

    working on audio settings UI

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ContinueSettings();
+    }
+    
+    private void ContinueSettings() 
+    { 
+        
+    }
+   
+}
