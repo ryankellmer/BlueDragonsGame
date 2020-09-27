6b3d266b6f5b76c049b8d5c0bbe06cf0640e8d36
Jason Eastman <jasont41@gmail.com>
2020-09-27 16:21:12 -0700

    Tower Spawning (tower_spawning.cs)
    Implemented Tower Spawn script, and created tower_pos prefab

---
+++ Assets/Scripts/tower_spawning.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower_spawning : MonoBehaviour
+{
+    public GameObject the_prefab;
+    bool already = false;
+
+    private void OnMouseDown()
+    {
+        check_tower(); 
+    }
+
+    void check_tower()
+    {
+        if(already == false)
+        {
+            Instantiate(the_prefab, transform.position, Quaternion.identity);
+            already = true; 
+        }
+    }
+}
