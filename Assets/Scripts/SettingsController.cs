9747a28969841ced16328a7df1071b0f66fd6570
ZionJones <Joneszion412@gmail.com>
2020-10-11 01:40:15 -0700

    settings update

---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+
+    public GameObject Soundcanvas;
+    private bool isShowing = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isShowing)
+            {
+                Soundcanvas.SetActive(false);
+                isShowing = false;
+            }
+        }
+
+        // Update is called once per frame
+    }
+}
