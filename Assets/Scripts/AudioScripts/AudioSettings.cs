6e7e29021d4ce794c9518fe89343594532341d2b
ZionJones <Joneszion412@gmail.com>
2020-10-13 03:26:09 -0700

    finished Sound setting sound will not be saved threw ought all scenes

--- Assets/Scripts/AudioScripts/AudioSettings.cs
+++ Assets/Scripts/AudioScripts/AudioSettings.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+
 using UnityEngine;
 
 public class AudioSettings : MonoBehaviour
 {
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPRef";
+
+    public float backgroundFloat, soundEffectFloat;
+
+    public AudioSource backgroundAudio;
+    public AudioSource[] soundEffectAudio; //can set to however many audio sources you need assigned in GUI
+
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -11,8 +20,19 @@ public class AudioSettings : MonoBehaviour
     }
     
     private void ContinueSettings() 
-    { 
+    {
+        backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+        soundEffectFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
         
+        
+        
+        
+        backgroundAudio.volume = backgroundFloat;
+
+        for (int i = 0; i < soundEffectAudio.Length; i++)
+        {
+            soundEffectAudio[i].volume = soundEffectFloat;
+        }
     }
    
 }
