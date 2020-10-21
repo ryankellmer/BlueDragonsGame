3e423156c00e84fede4fef3f6408996740a910f0
blakemardon <58799121+blakemardon@users.noreply.github.com>
2020-10-20 23:36:37 -0700

    Revert "TowerPositions prefab will now be added to the scene if not there already."
    This reverts commit 6fcfcb73243280bfd09e047aa3b1668866494d74.

--- Assets/Scripts/GenerateScenary.cs
+++ Assets/Scripts/GenerateScenary.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.Tilemaps;
 using Random = System.Random;
 
@@ -26,19 +25,7 @@ public class GenerateScenary : MonoBehaviour
                 }
             }
         }
-<<<<<<< Updated upstream
        GameObject.Find("TowerPositions").GetComponent<TowerPositions>().CreateTowerPositions();
-=======
-       GameObject tp = GameObject.Find("TowerPositions");
-       if(tp == null){
-           GameObject tpPrefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/TowerPositions.prefab", typeof(GameObject));
-           tp = Instantiate(tpPrefab, Vector3.zero, Quaternion.identity);
-           tp.GetComponent<TowerPositions>().CreateTowerPositions();
-       }
-       else{
-           tp.GetComponent<TowerPositions>().CreateTowerPositions();
-       }
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
