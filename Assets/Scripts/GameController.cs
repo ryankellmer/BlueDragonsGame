25e5e47ffb105b4293bfe4976f99359490182ad2
cdunham927 <cdunham927@gmail.com>
2020-10-22 14:59:02 -0700

    Numbers 1, 2, 3, and 8 increase game speed to their respective numbers

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -63,6 +63,26 @@ public class GameController : MonoBehaviour
             {
                 AddMoney(50);
             }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Time.timeScale = 3f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Time.timeScale = 1f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Time.timeScale = 2f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                Time.timeScale = 8f;
+            }
         }
 
         if (health <= 0) GameOver();
