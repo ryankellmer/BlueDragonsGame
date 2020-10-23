e2d3afd688a82c8bfe7ddf8311ab8924ccf02d56
ZionJones <Joneszion412@gmail.com>
2020-10-22 23:03:09 -0700

    added pause function

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,7 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void LoadScene(string SceneName)
+   
+    public GameObject PauseUI;
+    public GameObject SoundOptionMenuUI;
+    
+    
+
+
+    void Start() 
+   {
+        
+        //UI.SetActive(false);
+        
+   
+   }
+    
+  void Update() 
+  { 
+    
+   
+        if(PauseUI.gameObject.activeInHierarchy == true) 
+        {
+            Pause();
+            if (SoundOptionMenuUI.gameObject.activeInHierarchy == true) 
+            {
+                Pause();
+            }
+        }
+        
+        
+        
+        else
+        {
+            Resume();
+        }
+
+        
+
+
+
+  } 
+    
+    
+   public void LoadScene(string SceneName)
    {
        SceneManager.LoadScene(SceneName);
    }
@@ -19,5 +61,16 @@ public class MainMenuController : MonoBehaviour
     public void RestartGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Pause() 
+    {
+        //PauseUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+   
+    public void Resume() 
+    {
+        Time.timeScale = 1;
     }
 }
