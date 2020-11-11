53f21d5172646dc046d9fbb8abcaa79a4cd89b1e
ZionJones <Joneszion412@gmail.com>
2020-11-10 20:56:45 -0800

    working on audio manger update

---
+++ Assets/Scripts/AudioMixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMixer : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public AudioSource BGM;
+    public AudioSource[] SFX;
+
+
+
+    public void SetValue(float sliderValue) 
+    { 
+    
+    }
+
+    public voids
+
+
+
+
+
+
+
+
+
+}
