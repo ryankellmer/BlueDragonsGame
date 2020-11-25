163ed865643be32cc035c3ad2fa24cd3988fc5dc
ZionJones <Joneszion412@gmail.com>
2020-11-24 22:54:33 -0800

    new vfx

---
+++ Assets/Scripts/Enemy Scripts/EnemyFlash.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlash : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnCollsionEnter2D(Collision2D collision) 
+        { 
+            if (collision.gameObject.tag == "Bomb") 
+            {
+
+                Enemy.GetComponent<Animator>().Play("Flash");
+            
+            }
+        }
+        
+    }
+}
