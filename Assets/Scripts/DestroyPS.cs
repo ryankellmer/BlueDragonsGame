3e637379c087780fabd2836ca99ada05348d883d
ZionJones <Joneszion412@gmail.com>
2020-11-30 22:59:48 -0800

    paritcle system permorfamnce (destory)

---
+++ Assets/Scripts/DestroyPS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, GetComponent<ParticleSystem> ().main.duration + GetComponent<ParticleSystem> ().main.startLifetime.constantMax); 
+    }
+
+    
+}
